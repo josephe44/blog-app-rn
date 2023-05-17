779a852bdad1940aca1a3a04d63788c150db2de7
josephe44 <josephe442@gmail.com>
2023-05-17 15:05:08 +0100

    first commit

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { Context } from "../context/BlogContext";
+
+export default function ShowScreen({ navigation }) {
+  const { state } = useContext(Context);
+  const id = navigation.getParam("id");
+
+  const blogPost = state.find((blogPost) => blogPost.id === id);
+
+  return (
+    <View style={styles.container}>
+      <Text>ShowScreen</Text>
+      <Text>{blogPost.title}</Text>
+    </View>
+  );
+}
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("Edit", { id: navigation.getParam("id") })
+        }
+        style={styles.addIcon}
+      >
+        <Feather name="edit" size={24} color="black" />
+      </TouchableOpacity>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    // justifyContent: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  addIcon: {
+    paddingRight: 20,
+  },
+});
